c3dc714430818d461c19832577eab42d351b7365
bemery2 <bemery2@missouriwestern.edu>
2022-08-20 22:49:28 -0500

    Framework-Overhall/Bug-Fixes/Fixed-Promise-Usage

--- src/RequiredPackages/Adapter.lua
+++ src/RequiredPackages/Adapter.lua
@@ -1,4 +1,7 @@
 local RunService = game:GetService('RunService');
+local PlayerService = game:GetService('Players');
+local Promise = require(script.Parent.Promise);
+local Serialization = require(script.Parent.Serialization);
 
 local IsServer = RunService:IsServer();
 
@@ -9,37 +12,54 @@ function Adapter:_CheckMiddleware(Category: string, Args: {[any]: any})
 
 end
 
--- [[ This will pertain to middleware exclusively ]] --
-function Adapter:_CreatePlayerStatistics(Player: Player)
-    local Statistics = {
-        Caller = Player;
+if IsServer then
+    export type PlayerStatistic = {
+        Caller: Player;
+        ["1"]: {[string]: {[string]: any}};
+        ["2"]: {[string]: {[string]: any}}
+    }
 
-        [self._RFunction] = {
-            Requests = {
-                Success = 0;
-                Total = 0;
+    -- [[ This will pertain to middleware exclusively ]] --
+    function Adapter:_CreatePlayerStatistics(Player: Player)
+        local Statistics = {
+            Caller = Player;
+
+            [1] = {
+                Requests = {
+                    Success = 0;
+                    Total = 0;
+                };
+                Last = {
+                    Time = tick() - 10;
+                    Status = false;
+                }
             };
-            Last = {
-                Time = tick() - 10;
-                Status = false;
+
+            [2] = {
+                Requests = {
+                    Success = 0;
+                    Total = 0;
+                };
+                Last = {
+                    Time = tick() - 10;
+                    Status = false;
+                }
             }
         };
 
-        [self._REvent] = {
-            Requests = {
-                Success = 0;
-                Total = 0;
-            };
-            Last = {
-                Time = tick() - 10;
-                Status = false;
-            }
-        }
-    };
+        self._PlayerStatistics[Player] = Statistics;
 
-    self._PlayerStatistics[Player] = Statistics;
+        return Statistics :: PlayerStatistic;
+    end
 
-    return Statistics;
+    function Adapter:Fire(Filter: (Player) -> boolean, Event: string, ...)
+        local Args = {...};
+        for _, Player in pairs(PlayerService:GetPlayers()) do
+            if Filter(Player) then
+                self._REvent:FireClient(Player, Event, Args);
+            end
+        end
+    end
 end
 
 function Adapter.new(Parent, Provider)
@@ -47,14 +67,32 @@ function Adapter.new(Parent, Provider)
     self._Events = {};
     self._Functions = {};
     self._Middleware = {
-        ["Inbound"] = {};
-        ["Outbound"] = {};
+        ["Inbound"] = {
+            [1] = function(Args)
+                if IsServer then
+                    table.remove(Args, 1);
+                end
+                Args = Serialization:DeserializeData(Args);
+                return true, Args;
+            end
+        };
+        ["Outbound"] = {
+            [1] = function(Args)
+                if IsServer then
+                    table.remove(Args, 1);
+                end
+
+                Args = Serialization:SerializeData(Args);
+                return true, Args;
+            end
+        };
     }
     self._PlayerStatistics = {};
 
     if IsServer then
         assert(typeof(Provider) == "table", "Table is expected for Server Handler");
-        if not Provider.Client then return end
+        assert(Provider.Client, "Client table couldn't found.");
+        assert(typeof(Provider.Client) == "table", "Client data provided is not of type 'table'.")
 
         local Folder = Instance.new("Folder");
         Folder.Name = Provider.Name;
@@ -77,27 +115,27 @@ function Adapter.new(Parent, Provider)
                 Statistics = self:_CreatePlayerStatistics(Player);
             end
 
-            if Todo == "IsValid" then
-                local Event = table.remove(Args, 1);
+            if Todo == "GetEvents" then
+                local List = {};
+                for Index in pairs(Provider.Client) do
+                    if Index ~= "Server" then
+                        table.insert(List, Index);
+                    end
+                end
                 Res.Success = true;
-                Res.Response = Provider.Client[Event];
+                Res.Response = List;
             elseif Provider.Client[Todo] then
-                if #self._Middleware["Inbound"] == 0 then
+                table.insert(Args, 1, Statistics);
+                local Passed;
+                Passed, Args = self:_CheckMiddleware("Inbound", Args);
+
+                if Passed then
                     Res.Success = true;
                     Res.Response = Provider.Client[Todo](Provider.Client, IsServer and Player or nil, unpack(Args));
-                else
-                    table.insert(Args, 1, Statistics);
-                    local Passed;
-                    Passed, Args = self:_CheckMiddleware("Inbound", Args);
-
-                    if Passed then
-                        Res.Success = true;
-                        Res.Response = Provider.Client[Todo](Provider.Client, IsServer and Player or nil, unpack(Args));
-                    end
                 end
             end
 
-            local CurrentStatistics = Statistics[RFunction];
+            local CurrentStatistics = Statistics[1];
             CurrentStatistics.Requests.Total += 1;
             CurrentStatistics.Requests.Success += if Res.Success then 1 else 0;
             CurrentStatistics.Last.Time = if Res.Success then tick() else CurrentStatistics.Last.Time;
@@ -105,38 +143,6 @@ function Adapter.new(Parent, Provider)
 
             return Res;
         end
-
-        REvent.OnServerEvent:Connect(function(Player, Todo, Args)
-            local Success = false;
-
-            local Statistics = self._PlayerStatistics[Player];
-
-            if not Statistics then
-                Statistics = self:_CreatePlayerStatistics(Player);
-            end
-        
-            if Provider.Client[Todo] then
-                if #self._Middleware["Inbound"] == 0 then
-                    Success = true;
-                    task.spawn(Provider.Client[Todo], Provider.Client, IsServer and Player or nil, unpack(Args));
-                else
-                    table.insert(Args, 1, Statistics);
-                    local Passed;
-                    Passed, Args = self:_CheckMiddleware("Inbound", Args);
-
-                    if Passed then
-                        Success = true;
-                        task.spawn(Provider.Client[Todo], Provider.Client, IsServer and Player or nil, unpack(Args));
-                    end
-                end
-            end
-
-            local CurrentStatistics = Statistics[REvent];
-            CurrentStatistics.Requests.Total += 1;
-            CurrentStatistics.Requests.Success += if Success then 1 else 0;
-            CurrentStatistics.Last.Time = if Success then tick() else CurrentStatistics.Last.Time;
-            CurrentStatistics.Last.Status = Success;
-        end)
     else
         assert(typeof(Provider) == "string", "String is expected for Client Handler");
         local Folder = Parent:FindFirstChild(Provider);
@@ -144,6 +150,39 @@ function Adapter.new(Parent, Provider)
 
         self._REvent = Folder:FindFirstChild("RemoteEvent");
         self._RFunction = Folder:FindFirstChild("RemoteFunction");
+
+        local Res = self._RFunction:InvokeServer("GetEvents");
+        if Res.Success then
+            self.Events = Res.Response;
+
+            for _, Event in pairs(self.Events) do
+                local function Call(...)
+                    local Args = Serialization:SerializeData({...});
+
+                    return Promise.new(function(Resolve, Reject)
+                        local Res;
+                        local Success = pcall(function()
+                            Res = self._RFunction:InvokeServer(Event, Args);
+                        end)
+
+                        if Success and Res and Res.Response then
+                            Res.Response = Serialization:DeserializeData(Res.Response);
+                        end
+
+                        return Res.Success and Resolve(Res.Response) or Reject();
+                    end);
+                end
+                
+                self[Event .. "Async"] = function(_, ...)
+                    return Call(...);
+                end
+                
+                self[Event] = function(_, ...)
+                    local _, Res = Call(...):await();
+                    return Res;
+                end
+            end
+        end
     end
 
     return self;
--- src/init.lua
+++ src/init.lua
@@ -28,6 +28,8 @@ end
 local Promise = Resources["Promise"];
 local Janitor = Resources["Janitor"];
 local Signal = Resources["Signal"];
+local Adapter = Resources["Adapter"];
+
 local IsServer = RunService:IsServer();
 local Handler;
 
@@ -47,18 +49,18 @@ function Cardinal.new()
         [PlayerService] = {};
     };
 
-    self._Networking = true;
-    self._Performance = true;
 
-    if self._Networking then
-        if IsServer then
-            local Folder = Instance.new("Folder");
-            Folder.Name = "Services";
-            Folder.Parent = script;
-            self._ServiceFolder = Folder;
-        else
-            self._ServiceFolder = script:WaitForChild("Services");
-        end
+    local Folder;
+    if IsServer then
+        self._Networking = true;
+        self._Performance = true;
+
+        Folder = Instance.new("Folder");
+        Folder.Name = "ServiceFolder";
+        Folder.Parent = script;
+        self._ServiceFolder = Folder;
+    else
+        self._ServiceFolder = script:WaitForChild("ServiceFolder");
     end
 
     local _Janitor = self._Janitor;
@@ -69,66 +71,80 @@ function Cardinal.new()
     return self;
 end
 
-local Custom = {};
-Custom.__index = Custom;
-Custom.ClassName = IsServer and "Service" or "Controller";
-Custom.__tostring = function()
-    return Custom.ClassName;
-end
-
 if IsServer then
     -- [[ Server Exclusive ]] --
     function Cardinal:CreateService(ServiceInfo: {[any]: any})
         assert(ServiceInfo.Name, "Service Requires Name");
         local NewService = ServiceInfo;
-        NewService.Dependencies = {};
 
-        if self._Networking and NewService.Client then
-            local Adapter = Resources["Adapter"];
-
-            if Adapter then
-                NewService._NetworkAdapter = Adapter.new(NewService);
-
-                if typeof(NewService.Client) ~= "table" then
-                    NewService.Client = {Server = NewService};
-                else
-                    NewService.Client.Server = NewService;
-                end
+        if self._Networking and Adapter and NewService.Client ~= nil then
+            if typeof(NewService.Client) ~= "table" then
+                NewService.Client = {Server = NewService};
+            else
+                NewService.Client.Server = NewService;
             end
+
+            NewService._Network = Adapter.new(self._ServiceFolder, NewService);
         end
 
         Runnables[NewService.Name] = NewService;
         return NewService;
     end
 else
+    local Adapters = {};
+
+    function Cardinal:GetServiceAsync(Name: string)
+        if not self._Networking then
+            return Promise.reject("Networking isn't enabled.");
+        end
+
+        if Adapters[Name] then
+            return Promise.resolve(Adapters[Name]);
+        else
+            return Promise.new(function(Resolve, Reject)
+                local Adapter = Adapter.new(self._ServiceFolder, Name);
+                if not Adapter then
+                    return Reject("Adapter couldn't be made.");
+                end
+                Adapters[Name] = Adapter;
+                return Resolve(Adapter);
+            end)
+        end
+    end
+
+    function Cardinal:GetService(Name: string)
+        local _, NetworkAdapter = self:GetServiceAsync(Name):catch(warn):await();
+        return NetworkAdapter;
+    end
     -- [[ Client Exclusive ]] --
     function Cardinal:CreateController(ControllerInfo: {[any]: any})
         assert(ControllerInfo.Name, "Controller Requires Name");
         local NewController = ControllerInfo;
-        NewController.Dependencies = {};
         Runnables[NewController.Name] = NewController;
         return NewController;
     end
 end
 
 -- [[ Generics ]] --
-local function GetAllDescendants(Data: Folder | {ModuleScript} | ModuleScript)
-    local Table;
+local function Get(Data: Folder | {ModuleScript} | ModuleScript, Descendants: boolean?)
+    local T = if typeof(Data) == "table" then Data else Data:GetChildren();
 
-    if typeof(Data) == "Instance" then
-        Table = {Data, Data:GetDescendants()};
-    else
-        Table = Data;
-        for _, Object in pairs(Data) do
-            if typeof(Object) == "Instance" then
-                for _, Descendant in pairs(Object:GetDescendants()) do
-                    Table = {unpack(Table), Descendant};
+     if T and Descendants then
+        for Index, Object in pairs(T) do
+            if not Object:IsA("ModuleScript") then
+                table.remove(T, table.find(T, Object));
+                continue
+            end
+            
+            for _, Descendant in pairs(Object:GetDescendants()) do
+                if Descendant:IsA("ModuleScript") then
+                    table.insert(T, Descendant);
                 end
             end
         end
     end
 
-    return Table;
+    return T;
 end
 
 local function ProcessObject(Object, Table: {any}?)
@@ -137,45 +153,25 @@ local function ProcessObject(Object, Table: {any}?)
         if Table then
             Table[Object.Name] = require(Object);
         else
-            require(Object.Name);
+            require(Object);
         end
         table.remove(LoadCache, table.find(LoadCache, Object.Name));
     end
 end
 
-function Cardinal:AddResources(_Resources: Folder | {ModuleScript} | ModuleScript, Descendants: boolean?)
-    if Descendants then
-        _Resources = GetAllDescendants(_Resources);
-    else
-        if typeof(_Resources) == "Instance" then
-            if _Resources:IsA("Folder") then
-                _Resources = _Resources:GetChildren();
-            else
-                _Resources = { _Resources };
-            end
+function Cardinal:AddResources(_Resources: Folder | {ModuleScript} | ModuleScript | nil, Descendants: boolean?)
+    if _Resources ~= nil and typeof(_Resources) ~= "boolean" then
+        for _, Module in pairs(Get(_Resources, Descendants)) do
+            task.spawn(ProcessObject, Module, Resources);
         end
-    end
-
-    for _, Module in pairs(_Resources) do
-        task.spawn(ProcessObject, Module, Resources);
     end
 end
 
-function Cardinal:AddRunnables(_Runnables: Folder | {ModuleScript} | ModuleScript, Descendants: boolean?)
-    if Descendants then
-        _Runnables = GetAllDescendants(_Runnables);
-    else
-        if typeof(_Runnables) == "Instance" then
-            if _Runnables:IsA("Folder") then
-                _Runnables = _Runnables:GetChildren();
-            else
-                _Runnables = { _Runnables };
-            end
+function Cardinal:AddRunnables(_Runnables: Folder | {ModuleScript} | ModuleScript | nil, Descendants: boolean?)
+    if _Runnables ~= nil and typeof(_Runnables) ~= "boolean" then
+        for _, Module in pairs(Get(_Runnables, Descendants)) do
+            task.spawn(ProcessObject, Module);
         end
-    end
-
-    for _, Module in pairs(_Runnables) do
-        task.spawn(ProcessObject, Module);
     end
 end
 
@@ -183,16 +179,18 @@ function Cardinal:AddEvent(Service: ServiceProvider, Event: string, Callback: (.
     local ProviderCache = self._Events[Service];
     if not ProviderCache then return nil end
 
-    local LogicalEvent;
-    local Success = pcall(function()
-        LogicalEvent = Service[Event];
-    end)
-
-    if not Success then return nil end
     if not self._Events[Service][Event] then
         local Table = {};
 
         if Service ~= PlayerService then
+            local LogicalEvent;
+            
+            local Success = pcall(function()
+                LogicalEvent = Service[Event];
+            end)
+        
+            if not Success then return nil end
+
             self._Janitor:Add(LogicalEvent:Connect(function(...)
                 for _, _Callback in pairs(Table) do
                     task.spawn(_Callback, ...);
@@ -245,8 +243,11 @@ function Cardinal:_Init()
         Runnable.__Clock = os.clock();
 
         if Runnable.OnInit ~= nil then
-            table.insert(Promises, Promise.new(function(Resolve, Reject)
-                local Success, Error = pcall(Runnable.OnInit, Runnable);
+            table.insert(Promises, Promise.async(function(Resolve, Reject)
+                local Success, Error = pcall(function()
+                    Runnable:OnInit();
+                end)
+
                 if not Success then
                     warn(string.format("%s Couldn't be Initialized: %s", Name, tostring(Error)));
                     return Reject();
@@ -260,38 +261,40 @@ function Cardinal:_Init()
         end
     end
 
-    local Finished = Promise.allSettled(Promises);
-
-    if Finished.Status == Promise.Status.Rejected then
-        return Promise.reject();
-    else
-        self.Initialized:Fire();
-        return Promise.resolve();
-    end
+    return Promise.allSettled(Promises);
 end
 
 function Cardinal:Start(Config: {[string]: any}?)
-    if Config then
-        self._Networking = Config.Networking ~= nil and Config.Networking or true;
-        self._Performance = Config.Performance ~= nil and Config.Performance or true;
-    end
+    if IsServer then
+        if Config then
+            self._Networking = Config.Networking ~= nil and Config.Networking or true;
+            self._Performance = Config.Performance ~= nil and Config.Performance or true;
+        end
 
-    if not IsServer then
+        if not self._Networking then
+            self._ServiceFolder:Destroy();
+        end
+    else
+        if Config then
+            self._Performance = Config.Performance ~= nil and Config.Performance or true;
+        end
         script:WaitForChild("ServerReady");
+        self._Networking = if script:FindFirstChild("ServiceFolder") then true else false;
     end
 
-    local Initialized = self:_Init();
-
-    if Initialized.Status == Promise.Status.Rejected then
-        return Promise.reject("Initialization Failed");
+    local InitSuccessful = self:_Init():await();
+    if not InitSuccessful then
+        return Promise.reject("Couldn't Initialize all Runnables.");
     end
 
     local Promises = {};
 
     for Name, Runnable in pairs(Runnables) do
         if Runnable.OnStart ~= nil then
-            table.insert(Promises, Promise.new(function(Resolve, Reject)
-                local Success, Error = pcall(Runnable.OnStart, Runnable);
+            table.insert(Promises, Promise.async(function(Resolve, Reject)
+                local Success, Error = pcall(function()
+                    Runnable:OnStart();
+                end);
                 if not Success then
                     warn(string.format("%s Couldn't be Started: %s", Name, tostring(Error)));
                     return Reject();
@@ -306,10 +309,10 @@ function Cardinal:Start(Config: {[string]: any}?)
         end
     end
 
-    local Started = Promise.allSettled(Promises);
+    local Started = Promise.allSettled(Promises):await();
 
-    if Started.Status == Promise.Status.Rejected then
-        return Promise.reject();
+    if not Started then
+        return Promise.reject("Couldn't Start all Runnables.");
     end
 
     for _, Runnable in pairs(Runnables) do
@@ -334,25 +337,29 @@ function Cardinal:Start(Config: {[string]: any}?)
         end
     end
 
-    local function PlayerAdded(Player)
-        for _, Callback in pairs(self._Events[PlayerService]["PlayerAdded"]) do
-            task.spawn(Callback, Player);
-        end
+	local function PlayerAdded(Player)
+		if self._Events[PlayerService]["PlayerAdded"] then
+        	for _, Callback in pairs(self._Events[PlayerService]["PlayerAdded"]) do
+            	task.spawn(Callback, Player);
+			end
+		end
 
         local _Janitor = Janitor.new();
         _Janitor:LinkToInstance(Player);
 
-        local function CharacterAdded(Character)
-            if not Character:IsDescendantOf(workspace) then
-                repeat
-                    task.wait(.05);
-                until Character:IsDescendantOf(workspace) and Character.PrimaryPart;
-            end
+		local function CharacterAdded(Character)
+			if self._Events[PlayerService]["CharacterAdded"] then
+	            if not Character:IsDescendantOf(workspace) or Character.PrimaryPart then
+	                repeat
+	                    task.wait(.01);
+	                until Character:IsDescendantOf(workspace) and Character.PrimaryPart ~= nil;
+	            end
 
-            for _, Callback in pairs(self._Events[PlayerService]["CharacterAdded"]) do
-                task.spawn(Callback, Character, Player);
-            end
-        end
+	            for _, Callback in pairs(self._Events[PlayerService]["CharacterAdded"]) do
+	                task.spawn(Callback, Character, Player);
+	            end
+			end	
+		end
 
         if PlayerService.CharacterAutoLoads then
             local Character = Player.Character or Player.CharacterAdded:Wait();
@@ -365,7 +372,7 @@ function Cardinal:Start(Config: {[string]: any}?)
     self._Janitor:Add(PlayerService.PlayerAdded:Connect(PlayerAdded));
 
     for _, Player in pairs(PlayerService:GetPlayers()) do
-        task.spawn(PlayerAdded, Player);
+        PlayerAdded(Player);
     end
 
     if IsServer then
